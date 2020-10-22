572d6dd759783b05dfec5512e534b42e9cbd29d5
RSOULIAC <raphael.souliac@ynov.com>
2020-10-22 15:41:03 +0200

    script powershell 2

---
+++ tp1_script2.ps1
@@ -0,0 +1,20 @@
+﻿# script qui shutdown et lock le pc
+
+$func = $args[0]
+$time = $args[1]
+
+If ($func -eq 'lock')
+{
+Start-Sleep -Seconds $time
+rundll32.exe user32.dll,LockWorkStation
+
+}
+ElseIf ($func -eq 'shutdown')
+{
+Start-Sleep -Seconds $time
+Get-CimInstance -Classname Win32_OperatingSystem | Invoke-CimMethod -MethodName Shutdown
+}
+Else
+{
+Write-Output "Erreur"
+}
